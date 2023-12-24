77b81b87f93bd060213b69a08b5b8b887f6acf80
Abhay Patel <84718652+AbhayPatel1@users.noreply.github.com>
2023-12-24 18:06:52 +0000

    "ref: key and ' fix"

--- app/components/Review/ReviewClient.tsx
+++ app/components/Review/ReviewClient.tsx
@@ -40,7 +40,7 @@ const ReviewClient: React.FC<Props> = ({ reviews }) => {
   return (
     <>
       {reviews.slice(0, 4).map((review) => (
-        <ReviewCard review={review} formatPostedTime={formatPostedTime} />
+        <ReviewCard key={review.id} review={review} formatPostedTime={formatPostedTime} />
       ))}
 
       {showMoreButton && (
